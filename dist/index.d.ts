77a310f1851680544c18e2d562198690da9b4c71
Manthan <manthan.nadcab@gmail.com>
2024-09-30 11:43:56 +0530

    sds

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import JSBI from 'jsbi';
-export { JSBI };
-export { BigintIsh, ChainId, TradeType, Rounding, SolidityType, FACTORY_ADDRESS, INIT_CODE_HASH, MINIMUM_LIQUIDITY } from './constants';
-export * from './errors';
-export * from './entities';
-export * from './router';
-export * from './fetcher';
-export * from './utils';
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./xtring-sdk.cjs.production.min.js')
-} else {
-  module.exports = require('./xtring-sdk.cjs.development.js')
-}
